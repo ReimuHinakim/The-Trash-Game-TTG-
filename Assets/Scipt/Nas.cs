3fe263906f7d70095a6e2d8e9e386ccab2b3292b
ReimuHinakim <95842974+ReimuHinakim@users.noreply.github.com>
2022-02-03 20:18:41 -1000

    tutorial

---
+++ Assets/Scipt/Nas.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Nas : MonoBehaviour
+{
+    public void Hanna()
+    {
+        SceneManager.LoadScene("Tutorial3");
+    }
+}
