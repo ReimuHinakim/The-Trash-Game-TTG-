fa88e46c98a106b4ba646762cbcd0a9a9b8f631c
ReimuHinakim <95842974+ReimuHinakim@users.noreply.github.com>
2022-03-04 18:10:20 -1000

    CH two

--- Assets/Materials/Scipt/Choose.cs
+++ Assets/Materials/Scipt/Choose.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class Point : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class Choose : MonoBehaviour
 {
-
-    public Text PointText;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        PointText.text = "You Got " + Trash.Point.ToString() + " Point";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Slep()
+    {
+        SceneManager.LoadScene("Start(WAI)1");
     }
 }
---
+++ Assets/Materials/Scipt/TrashA.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrashA : MonoBehaviour
+{
+    public Text guideText;
+    public Text PointText;
+    public static int Point;
+    public string item;
+    public int itemNum;
+    public int[] itemNumbers;
+    public Text[] itemTextList;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PointText.text = Point.ToString();
+        itemTextList[0].text = itemNumbers[0].ToString();
+        itemTextList[1].text = itemNumbers[1].ToString();
+        itemTextList[2].text = itemNumbers[2].ToString();
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Can" && itemNum < 6)
+
+        {
+
+            item = "Can";
+            Destroy(collision.gameObject, 0.1f);
+            guideText.text = "Go trash can to throw away";
+            itemNumbers[0] += 2;
+            itemNum += 1;
+
+        }
+        if (collision.gameObject.tag == "Paper" && itemNum < 6)
+        {
+            item = "Paper";
+            Destroy(collision.gameObject, 0.1f);
+            guideText.text = "Go trash can to throw away";
+            itemNumbers[1] += 2;
+            itemNum += 1;
+
+
+        }
+        if (collision.gameObject.tag == "Hamburger" && itemNum < 6)
+        {
+            item = "Hamburger";
+            Destroy(collision.gameObject, 0.1f);
+            guideText.text = "Go trash can to throw away";
+            itemNumbers[2] += 2;
+            itemNum += 1;
+
+
+        }
+
+
+
+
+
+
+        if (collision.gameObject.tag == "CanTrash")
+        {
+
+            Point += itemNumbers[0];
+            itemNumbers[0] = 0;
+            Point += itemNumbers[1];
+            itemNumbers[1] = 0;
+            Point += itemNumbers[2];
+            itemNumbers[2] = 0;
+            guideText.text = "Go Get Trash";
+            itemNum = 0;
+
+        }
+
+
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Trash"))
+        {
+            guideText.text = "Pick Up";
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Trash"))
+        {
+            guideText.text = "Go Get Trash";
+        }
+    }
+
+}
